5aface8ba861bb60ba24d4392c9f363dfcdd091e
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2024-11-24 16:05:11 -0500

    update so condition isn't always true

--- src/app/harbor/battles/battles.tsx
+++ src/app/harbor/battles/battles.tsx
@@ -568,9 +568,11 @@ export default function Matchups({ session }: { session: HsSession }) {
                   }`}
                 >
                   {reason.trim().split(' ').length < 10 ? (
-                    reason.trim().split(' ') != [''] ? 
-                      (`${10 - reason.trim().split(' ').length} words left...`)
-                      : "10 words left..."
+                    reason.trim().split(' ') != [''] ? (
+                      `${10 - reason.trim().split(' ').length} words left...`
+                    ) : (
+                      '10 words left...'
+                    )
                   ) : isSubmitting ? (
                     <>
                       <svg
