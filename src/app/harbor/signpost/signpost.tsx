d7f379d99ccf33b75c7e7f21205c045df529a57d
OC <oc@theme-verse.com>
2024-12-21 13:54:42 +0530

    Update signpost.tsx
    Signed-off-by: OC <oc@theme-verse.com>

--- src/app/harbor/signpost/signpost.tsx
+++ src/app/harbor/signpost/signpost.tsx
@@ -170,7 +170,7 @@ export default function Signpost() {
         </p>
       </a>
 
-      <div className="flex items-center justify-center gap-4 overflow-x-scroll">
+      <div className="flex items-center justify-center gap-4">
         <div className="w-fit h-fit">
           <JaggedCard
             shadow={false}
