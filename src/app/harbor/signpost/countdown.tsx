7f792b027b2255130969fe03e54895725cd10cd1
MoPaMo <mopamo@users.noreply.github.com>
2025-01-07 20:55:19 +0100

    fix: correct hour display

--- src/app/harbor/signpost/countdown.tsx
+++ src/app/harbor/signpost/countdown.tsx
@@ -16,9 +16,7 @@ export default function Countdown() {
         return
       }
 
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-      )
+      const hours = Math.floor(distance / (1000 * 60 * 60))
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
       const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
