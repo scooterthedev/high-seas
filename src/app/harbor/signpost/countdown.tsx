3888ffa514dca8743238c1e431edcbf7c9d9e3cc
MoPaMo <mopamo@users.noreply.github.com>
2025-01-07 20:57:19 +0100

    feat:always display two digits for time

--- src/app/harbor/signpost/countdown.tsx
+++ src/app/harbor/signpost/countdown.tsx
@@ -20,7 +20,9 @@ export default function Countdown() {
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
       const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-      setTimeLeft(`${hours}:${minutes}:${seconds}`)
+      setTimeLeft(
+        `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`,
+      )
     }, 1000)
 
     return () => clearInterval(interval)
